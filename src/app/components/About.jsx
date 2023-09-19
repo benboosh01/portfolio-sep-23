cccc561ee96d446097bf5d51bda56ef9aef78404
benboosh01 <ben.bushell87@icloud.com>
2023-09-19 17:03:46 +0700

    update about text

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -3,7 +3,34 @@ const people = [
         name: "Ben Bushell",
         role: "Full Stack Web Developer / Sales Executive & Leader",
         imageUrl: "/profile_pic.jpeg",
-        bio: <>I'm an experienced sales professional, turned web developer. With over a decade in sales, I've consistently exceeded targets and developed lasting relationships with clients and colleagues alike. <br /> This background has shaped my ability to understand customer needs and deliver solutions that surpass expectations. I recently took on the challenge of learning web development, completing an intensive 3-month bootcamp. <br /> Since then, I've been working part-time for a charity, 'A Good Thing', and engaging in freelance projects that have enabled me to apply my diverse skill set. I specialize in JavaScript, NodeJS, and React, and I'm comfortable using a variety of other frameworks and tools such as NextJS, PostgreSQL, Firebase, Svelte, WordPress, CSS, Tailwind, Bootstrap, and VueJS. <br /> With a robust sales background, I possess a distinct perspective that bridges client needs with technical solutions. My commitment goes beyond just delivering on tasks; I aim to provide work that not only meets but exceeds your expectations. <br /> Currently, I'm keen on delving deeper into dynamic roles in this sector, with the dual goal of enhancing my expertise and contributing positively to the missions of my clients, employers and colleagues.</>,
+        bio: (
+            <>
+                I'm an experienced sales professional, turned web developer. With
+                over a decade in sales, I've consistently exceeded targets and
+                developed lasting relationships with clients and colleagues
+                alike.This background has shaped my ability to understand
+                customer needs and deliver solutions that surpass expectations.
+                <br />
+                I recently took on the challenge of learning web development,
+                completing an intensive 3-month bootcamp. Since then, I've been
+                working part-time for a charity, 'A Good Thing', and engaging in
+                freelance projects that have enabled me to apply my diverse
+                skill set. I'm comfortable using a variety of languages,
+                frameworks and tools such as JavaScript, NodeJS, React, NextJS,
+                PostgreSQL, Firebase, Svelte, WordPress, CSS, Tailwind,
+                Bootstrap, and VueJS, and am quick to learn with any task.
+                <br />
+                My sales background provides a perspective that bridges client
+                needs with technical solutions. My commitment goes beyond just
+                delivering on tasks; I aim to provide work that not only meets
+                but exceeds your expectations.
+                <br />
+                Currently, I'm keen on delving deeper into dynamic roles in this
+                sector, with the dual goal of enhancing my expertise and
+                contributing positively to the missions of my clients, employers
+                and colleagues.
+            </>
+        ),
         twitterUrl: "#",
         linkedinUrl: "#",
     },
