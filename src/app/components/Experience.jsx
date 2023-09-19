0f5e876fad38ec93686432b08a50df47e285a0e9
benboosh01 <ben.bushell87@icloud.com>
2023-09-19 17:03:54 +0700

    change spacing

--- src/app/components/Experience.jsx
+++ src/app/components/Experience.jsx
@@ -61,13 +61,13 @@ const posts = [
 
 export default function Experience() {
     return (
-        <div className="bg-white py-24 sm:py-32" id="experience">
+        <div className="bg-white py-16" id="experience">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center">
                         Experience
                     </h2>
-                    <ul className="mt-10 flex flex-col gap-6">
+                    <ul className="mt-6 flex flex-col gap-6">
                         {posts.map((post) => (
                             <article
                                 key={post.id}
